a1877411c67e80c032ac07b19a1ee56fa7f509b5
Matthew Corven <matthewcorven@gmail.com>
2024-03-09 22:44:30 -0500

    (chore) Update dependencies across full codebase

--- DaySpaPet.Web.Wasm/Program.cs
+++ DaySpaPet.Web.Wasm/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using DaySpaPet.Razor.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<DaySpaPet.Razor.Shared.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
